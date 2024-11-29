110035352788612781422db1b856960796482ce1
Samuel Andrade Teixeira <contatu@live.com>
2024-11-29 20:37:40 -0300

    foto

---
+++ NET/Test.cs
@@ -0,0 +1,8 @@
+int a = 10;
+int b = 3;
+
+Console.WriteLine("Soma: " + (a + b));     // 10 + 3 = 13
+Console.WriteLine("Subtração: " + (a - b)); // 10 - 3 = 7
+Console.WriteLine("Multiplicação: " + (a * b)); // 10 * 3 = 30
+Console.WriteLine("Divisão: " + (a / b));   // 10 / 3 = 3
+Console.WriteLine("Módulo: " + (a % b));    // 10 % 3 = 1
---
+++ NET/meuProjNET/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program2
+{
+    static void Main()
+    {
+
+        int i = 0;
+        while (i < 5) {
+            Console.WriteLine("Sim " + i + " é menor que 5");
+            i++;
+        }
+    }
+}
