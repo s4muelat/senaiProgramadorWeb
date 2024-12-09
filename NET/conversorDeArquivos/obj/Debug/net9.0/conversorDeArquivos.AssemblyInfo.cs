fff326f3e7a0bf96e3c7904917f95fa22bc42733
Samuel Andrade Teixeira <contatu@live.com>
2024-12-09 20:19:41 -0300

    Atualização geral

--- NET/conversorDeArquivos/obj/Debug/net9.0/conversorDeArquivos.AssemblyInfo.cs
+++ NET/conversorDeArquivos/obj/Debug/net9.0/conversorDeArquivos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("conversorDeArquivos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb157a2f12be5f73081a1de832035c2a60a7e77e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a200b9c155817656c409824eace204adc8ba290b")]
 [assembly: System.Reflection.AssemblyProductAttribute("conversorDeArquivos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("conversorDeArquivos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
